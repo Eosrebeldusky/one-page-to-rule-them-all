7ec460352980e5bc949431e559769bd37dba5b48
Eos Rebel Dusky <spando93@gmail.com>
2022-09-23 11:34:56 -0300

    Making  new work
    Get url making firs component again

---
+++ src/Pages/Components/MovieInfo.js
@@ -0,0 +1,13 @@
+import { toBeInTheDOM } from "@testing-library/jest-dom/dist/matchers";
+
+export function MoveInfo(props){
+    const [] = props
+
+    return(
+
+        <h1>{title}</h1>
+        <p>{}</p>
+    )
+
+
+}
